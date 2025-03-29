3bac3b53a01e6ca998c3c6cb1c7a925dc6cad957
Douglas Navas <douglas@navascogt.com>
2025-03-29 10:04:46 -0600

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,33 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.write('Hello Streamlit World!')
 
-st.write('Hello world!')
+with st.sidebar:
+    st.header('About famiologia')
+    st.write('This is my first app in Streamlit!')
+
+st.header('This is a header with a divider', divider='rainbow')
+
+st.markdown('This text is created with st.markdown')
+
+col1, col2 = st.columns(2)
+
+st.subheader('st.column')
+
+with col1:
+    x = st.slider('Select a value', 1, 20)
+
+with col2:
+    st.write(x, 'squared of :blue[***x***] is', x * x)
+
+st.subheader('st.line_chart')
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+st.subheader('st.area_chart')
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.area_chart(chart_data)
